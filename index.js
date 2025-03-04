3face26ade676260dd4f9615948178b0138488e9
Ghxstyl <127886995+Ghxstyl@users.noreply.github.com>
2025-03-04 14:13:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ app.get('/', (req, res) => {
   res.send('YaY Your Bot Status Changed✨');
 });
 app.listen(port, () => {
-  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
-  console.log(`🔗 Powered By RTX`);
+  console.log(`🔗 Ezt hallgatja: AlwaysFiveStack:${port}`);
+  console.log(`🔗 AlwaysFiveStack`);
 });
 
 
