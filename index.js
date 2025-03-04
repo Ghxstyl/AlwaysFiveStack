cac48c3c07d664ca78ae9607da0aca4b96abf0ed
Ghxstyl <127886995+Ghxstyl@users.noreply.github.com>
2025-03-04 12:23:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Hallgatja: AlwaysFiveStack", "🎮 Playing VALORANT"];
+const statusMessages = ["🎧 Hallgatja: AlwaysFiveStack", "🎮 Játszik ezzel: AlwaysFiveStack"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
